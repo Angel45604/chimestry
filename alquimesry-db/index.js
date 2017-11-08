69670308320904be67cc40bdcd58a483ed30b416
Angel Marcos <angel.mauricio.marcos@live.com>
2017-11-07 18:35:22 -0600

    Some changes

--- alquimesry-db/index.js
+++ alquimesry-db/index.js
@@ -1,31 +1,30 @@
 'use strict'
 
-const setupDatabase = require('lib/db')
+const setupDatabase = require('./lib/db')
 const setupUserModel = require('./models/user')
 const setupCompoundModel = require('./models/compound')
 
-
 module.exports = async function (config) {
-    const sequelize = setupDatabase(config)
-    const UserModel = setupUserModel(config)
-    const CompoundModel = setupCompoundModel(config)
+  const sequelize = setupDatabase(config)
+  const UserModel = setupUserModel(config)
+  const CompoundModel = setupCompoundModel(config)
 
-    UserModel.hasMany(CompoundModel)
-    CompoundModel.belongsTo(UserModel)
+  UserModel.hasMany(CompoundModel)
+  CompoundModel.belongsTo(UserModel)
 
-    await sequelize.authenticate()
+  await sequelize.authenticate()
 
-    if(config.setup) {
-        await sequelize.sync({ force: true })
-    }
+  if (config.setup) {
+    await sequelize.sync({ force: true })
+  }
 
-    sequelize.sync()
+  // sequelize.sync()
 
-    const User = {}
-    const Compound = {}
+  const User = {}
+  const Compound = {}
 
-    return {
-        User,
-        Compound
-    }
+  return {
+    User,
+    Compound
+  }
 }
--- alquimesry-db/models/user.js
+++ alquimesry-db/models/user.js
@@ -1,25 +1,24 @@
 'use strict'
 
 const Sequelize = require('sequelize')
-const setupDatabase = requie('../lib/db')
+const setupDatabase = require('../lib/db')
 
 module.exports = function setupUserModel (config) {
-    const sequelize = setupDatabase(config)
+  const sequelize = setupDatabase(config)
 
-
-    return sequelize.define('user', {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        username: {
-            type= Sequelize.STRING,
-            allowNull: false
-        },
-        password: {
-            type= Sequelize.STRING,
-            allowNull: false
-        }
-    })
+  return sequelize.define('user', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  })
 }
